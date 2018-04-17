ecd3469cfe53e4e9e7cd6c65338ffae15125fa61
John.Doe <33830954+iocaste@users.noreply.github.com>
2018-04-17 14:30:26 +0300

    Added subarray.

--- src/Transformer/Paginates.php
+++ src/Transformer/Paginates.php
@@ -27,18 +27,20 @@ trait Paginates
             )->withResourceName(false)->toArray(),
 
             'meta' => [
-                'total' => $paginator->total(),
-                'count' => $paginator->count(),
-                'per_page' => $paginator->perPage(),
-                'current_page' => $paginator->currentPage(),
-                'last_page' => $paginator->lastPage(),
-                'first_page_url' => $paginator->url(1),
-                'last_page_url' => $paginator->url($paginator->lastPage()),
-                'next_page_url' => $paginator->nextPageUrl(),
-                'prev_page_url' => $paginator->previousPageUrl(),
-                'from' => $paginator->firstItem(),
-                'to' => $paginator->lastItem(),
-                'has_more' => $paginator->hasMorePages(),
+                'pagination' => [
+                    'total' => $paginator->total(),
+                    'count' => $paginator->count(),
+                    'per_page' => $paginator->perPage(),
+                    'current_page' => $paginator->currentPage(),
+                    'last_page' => $paginator->lastPage(),
+                    'first_page_url' => $paginator->url(1),
+                    'last_page_url' => $paginator->url($paginator->lastPage()),
+                    'next_page_url' => $paginator->nextPageUrl(),
+                    'prev_page_url' => $paginator->previousPageUrl(),
+                    'from' => $paginator->firstItem(),
+                    'to' => $paginator->lastItem(),
+                    'has_more' => $paginator->hasMorePages(),
+                ],
             ],
         ];
     }
