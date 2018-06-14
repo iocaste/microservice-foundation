60cde3da51821c1fd70133a2cebc3e2bed6f094c
Ronalds <ronalds@web-design.lv>
2018-06-14 15:33:46 +0300

    Update helpers.php and load it in composer.json

--- src/helpers.php
+++ src/helpers.php
@@ -1,5 +1,8 @@
 <?php
 
+use Illuminate\Contracts\Debug\ExceptionHandler;
+use Symfony\Component\Debug\Exception\FatalThrowableError;
+
 if (! function_exists('bugsnag')) {
     /**
      * @return \Bugsnag\Client|\Laravel\Lumen\Application|mixed|bugsnag
@@ -33,5 +36,35 @@ if (! function_exists('request')) {
     function request()
     {
         return app('request');
+    }
+}
+
+if (! function_exists('to_boolean')) {
+    /**
+     * @param $value
+     *
+     * @return boolean
+     */
+    function to_boolean($value)
+    {
+        return filter_var($value, FILTER_VALIDATE_BOOLEAN);
+    }
+}
+
+if (! function_exists('report')) {
+    /**
+     * Report an exception.
+     *
+     * @param  \Exception  $exception
+     * @return void
+     */
+    function report($exception)
+    {
+        if ($exception instanceof Throwable &&
+            ! $exception instanceof Exception) {
+            $exception = new FatalThrowableError($exception);
+        }
+
+        app(ExceptionHandler::class)->report($exception);
     }
 }
