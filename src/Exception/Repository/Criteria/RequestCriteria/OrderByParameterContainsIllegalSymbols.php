bb7a1a85d6c563ed898b319ed2fc10d6620d0dc9
Vladimir <vladimir@web-design.lv>
2018-08-31 19:30:57 +0300

    Order by relations

---
+++ src/Exception/Repository/Criteria/RequestCriteria/OrderByParameterContainsIllegalSymbols.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Iocaste\Microservice\Foundation\Exception\Repository\Criteria\RequestCriteria;
+
+use Exception;
+
+/**
+ * Class OrderByParameterContainsIllegalSymbols
+ */
+class OrderByParameterContainsIllegalSymbols extends Exception
+{
+
+}
---
+++ src/Exception/Repository/Criteria/RequestCriteria/TryingToJoinUnavailableMethod.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Iocaste\Microservice\Foundation\Exception\Repository\Criteria\RequestCriteria;
+
+use Exception;
+
+/**
+ * Class TryingToJoinUnavailableMethod
+ */
+class TryingToJoinUnavailableMethod extends Exception
+{
+
+}
