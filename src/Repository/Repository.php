57e0373767525da8b3f348b5cfa14772a47fb1af
Vladimir <vladimir@web-design.lv>
2018-09-04 15:56:09 +0300

    OrderBy criteria moved to Filter repository

--- src/Repository/Repository.php
+++ src/Repository/Repository.php
@@ -3,6 +3,8 @@
 namespace Iocaste\Microservice\Foundation\Repository;
 
 use Prettus\Repository\Eloquent\BaseRepository;
+use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
+use Illuminate\Database\Eloquent\Model;
 
 /**
  * Class Repository.
@@ -17,5 +19,47 @@ abstract class Repository extends BaseRepository
         $this->model = $this->model->withTrashed();
 
         return $this;
+    }
+
+    /**
+     * @param string $column
+     * @param mixed $model
+     *
+     * @return string
+     */
+    public function getColumnName($column, $model = null)
+    {
+        return $column;
+    }
+
+    /**
+     * @param array $columns
+     * @param mixed $model
+     *
+     * @return array
+     */
+    public function getColumnsNames(array $columns, $model = null): array
+    {
+        return array_map(function ($column) use ($model) {
+            return $this->getColumnName($column, $model);
+        }, $columns);
+    }
+
+    /**
+     * @param mixed $model
+     *
+     * @return string
+     */
+    public function getTableName($model = null)
+    {
+        $model = $model ?? $this->model;
+
+        if ($model instanceof Model) {
+            return $model->getTable();
+        } elseif ($model instanceof EloquentBuilder) {
+            return $model->getModel()->getTable();
+        }
+
+        return $model->from;
     }
 }
